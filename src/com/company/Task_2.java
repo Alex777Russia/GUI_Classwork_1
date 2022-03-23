0a00fa3394cd3300581734909826b49fdb19be57
Alex777Russia <shevalex777@mail.ru>
2022-03-23 20:45:59 +0300

    Реализовано задание 3 - площадь под графиком

--- src/com/company/Task_2.java
+++ src/com/company/Task_2.java
@@ -1,4 +1,9 @@
 package com.company;
 
 public class Task_2 {
+    public static double someFunction(double x) {
+        return Math.pow(x, 3.0) - Math.pow(x, 2.5) + x - 1.0;
+    }
+
+
 }
--- src/com/company/Task_3.java
+++ src/com/company/Task_3.java
@@ -1,4 +1,32 @@
 package com.company;
+import java.util.Scanner;
 
 public class Task_3 {
+    public static double functionF(double x, double index){
+        return (Math.pow(x, index + 1)) / (index + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Input first number");
+        int firstNumber = scan.nextInt();
+
+        System.out.println("Input second number");
+        int secondNumber = scan.nextInt();
+
+        if (firstNumber > secondNumber) {
+            int timeValue = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = timeValue;
+        }
+
+        double F_a = functionF(firstNumber, 4) + functionF(firstNumber, 2)
+                + functionF(firstNumber, 1) - 7 * firstNumber;
+        double F_b = functionF(secondNumber, 4) + functionF(secondNumber, 2)
+                + functionF(secondNumber, 1) - 7 * secondNumber;
+
+        System.out.println(F_b - F_a);
+    }
+
 }
